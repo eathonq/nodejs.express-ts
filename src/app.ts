f368aea4ac4154962053e947b85ad9713329b39f
eathonq <vangagh@live.cn>
2022-12-02 19:00:19 +0800

    中间件传递参数方案优化

--- src/app.ts
+++ src/app.ts
@@ -18,21 +18,28 @@ app.all('*', function (req, res, next) {
     }
 });
 
+// 全局设置 json 数据格式转换中间件
+// import bodyParser = require('body-parser');
+// const jsonParser = bodyParser.json();
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// app.use(jsonParser);
+// app.use(urlencodedParser);
+
 // 设置路由
 import { router as index } from './routes/index';
 app.use('/', index);
 
 import { RES_CODE } from './models/code';
-// 设置成功返回
+// 设置默认成功返回
 app.use((req, res, next) => {
     res.send({
         code: RES_CODE.SUCCESS,
         message: RES_CODE[RES_CODE.SUCCESS],
-        data: req.body
+        data: req.body.router_default_data
     });
 });
 
-// 设置错误返回
+// 设置默认错误返回
 app.use((err: RES_CODE, req: any, res: any, next: any) => {
     res.send({
         code: err || RES_CODE.ERROR,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,23 @@ export const router = express.Router();
 
 const random = Math.random();
 
+import { RES_CODE } from '../models/code';
+
 // 测试信息
 router.get('/info/:data', (req, res, next) => {
     const data = `${random} Hello, ${req.params.data}! time: ${new Date().toLocaleString()}`;
-    req.body = data;
-    next();
+    // 使用默认成功返回
+    // req.body.router_default_data = data;
+    // next();
+
+    // 使用自定义成功返回
+    res.send({
+        code: RES_CODE.SUCCESS,
+        message: RES_CODE[RES_CODE.SUCCESS],
+        data
+    });
 });
 
-import { RES_CODE } from '../models/code';
 // 测试错误
 router.get('/error', (req, res, next) => {
     next(RES_CODE.TEST_ERROR);
@@ -22,6 +31,6 @@ const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 router.post('/post', jsonParser, urlencodedParser, (req, res, next) => {
     const data = req.body;
-    req.body = data;
+    req.body.data = data;
     next();
 });
